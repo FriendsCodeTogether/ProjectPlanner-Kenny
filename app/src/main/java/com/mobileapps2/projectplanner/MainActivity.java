9348b66bbf4b7b853cd832c01d70229138946edc
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2020-11-15 17:49:17 +0100

    removed bugg

--- app/src/main/java/com/mobileapps2/projectplanner/MainActivity.java
+++ app/src/main/java/com/mobileapps2/projectplanner/MainActivity.java
@@ -22,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       view) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
