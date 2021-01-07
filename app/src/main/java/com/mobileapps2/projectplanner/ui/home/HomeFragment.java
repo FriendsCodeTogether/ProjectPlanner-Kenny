afd60a1254d9c35151158f66f5098990318cbeb7
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2021-01-08 00:28:01 +0100

    added buttons to move from homepage

--- app/src/main/java/com/mobileapps2/projectplanner/ui/home/HomeFragment.java
+++ app/src/main/java/com/mobileapps2/projectplanner/ui/home/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.mobileapps2.projectplanner.ui.home;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,23 +17,32 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.mobileapps2.projectplanner.R;
 import com.mobileapps2.projectplanner.ui.boards.BoardActivity;
+import com.mobileapps2.projectplanner.ui.teams.TeamActivity;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private ImageButton navigateTeamsBtn;
+    private ImageButton navigateBoardsBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        navigateBoardsBtn = (ImageButton) root.findViewById(R.id.NavigateBoardBtn);
+        navigateTeamsBtn = (ImageButton) root.findViewById(R.id.NavigateTeamBtn);
+
+        navigateBoardsBtn.setOnClickListener((v -> {
+            Intent intent = new Intent(getActivity(),BoardActivity.class);
+            startActivity(intent);
+        }));
+
+        navigateTeamsBtn.setOnClickListener((v -> {
+            Intent intent = new Intent(getActivity(), TeamActivity.class);
+            startActivity(intent);
+        }));
         return root;
     }
 }
