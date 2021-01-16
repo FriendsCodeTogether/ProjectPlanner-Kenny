e9164c3c1d6b3cf9b158eadc031c947c41362e17
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2021-01-16 13:46:56 +0100

    added user registration pages

---
+++ app/src/main/java/com/mobileapps2/projectplanner/ui/Startup/StartupActivity.java
@@ -0,0 +1,35 @@
+package com.mobileapps2.projectplanner.ui.Startup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.mobileapps2.projectplanner.R;
+import com.mobileapps2.projectplanner.ui.teams.TeamListActivity;
+
+public class StartupActivity extends AppCompatActivity {
+private Button loginButton;
+private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+        loginButton = findViewById(R.id.LoginButton);
+        registerButton = findViewById(R.id.RegisterButton);
+
+        loginButton.setOnClickListener((v->{
+            //TODO: Remove this comment to continue on login
+            Intent intent = new Intent(this, TeamListActivity.class);
+            //Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }));
+        registerButton.setOnClickListener((v->{
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        }));
+    }
+}
