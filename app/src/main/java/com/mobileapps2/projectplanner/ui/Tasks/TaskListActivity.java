6130a63c874e35ad412a5b15eb2ef3500feb0c7b
Kenny-Lacroix <62295602+Kenny-Lacroix@users.noreply.github.com>
2021-01-17 16:49:40 +0100

    imported class

--- app/src/main/java/com/mobileapps2/projectplanner/ui/Tasks/TaskListActivity.java
+++ app/src/main/java/com/mobileapps2/projectplanner/ui/Tasks/TaskListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mobileapps2.projectplanner.R;
+
 public class TaskListActivity extends AppCompatActivity {
 
     @Override
